2d47f1f7d6fb662f7b2cea657a5b81ca15085a31
Jeremy Foster <jeremy.foster@live.com>
2018-05-17 15:30:49 -0700

    cascade options, add mock sensor functionality

--- index.ts
+++ index.ts
@@ -1,22 +1,38 @@
-// const five = require("johnny-five");
-// const Raspi = require('raspi-io');
 let WebSocket = require('ws');
-const config = require('./device.json');
-const argv = require('yargs').argv
+let config = require('./device.json');
+const commandLineArgs = require('command-line-args')
+
+//override config with command line options  
+let args = [
+    { name: 'websocketUrl', alias: 'u', required: true },
+    { name: 'deviceName', alias: 'd', required: true },
+    { name: 'friendlyName', alias: 'n', required: true }
+];
+config = { ...config, ...commandLineArgs(args) };
+
+//throw errors if any required arguments are missing
+args.filter(a => a.required && !config[a.name]).forEach(a => {
+    throw new Error(`A ${a.name} argument must be provided either in a device.json file or as a command line argument.`);
+})
 
 //setup websockets
 const ws = new WebSocket(config.websocketUrl);
 
-//setup j5
-// let board = new five.Board({ io: new Raspi() });
-//ready the board and the websocket connection
-// board.on("ready", () => {
-    ws.onopen = () => {
-        //just send hard coded messages every second
-        setInterval(() => sendDeltaMessage(config.deviceName, "environment.temperature", 68), 1000);
-        setInterval(() => sendDeltaMessage(config.deviceName, "environment.humidity", 40), 1200);
-    };
-// });
+ws.onopen = () => {
+    //send mock datapoints
+    mockSensor("environment.temperature",68,5,3000);
+    mockSensor("environment.humidity",40,3,3000);
+};
+
+//make it easy to create mock datapoints and send them as delta messages
+function mockSensor(datapoint:string, median: number, variance: number, frequency: number) {
+    let value;
+    setInterval(() => {
+        value = (median - variance) + Math.round(Math.random() * variance);
+        console.log(`Sending ${value} for ${datapoint}`);
+        sendDeltaMessage(config.deviceName, datapoint, value)
+    }, frequency)
+}
 
 function sendDeltaMessage(deviceId: string, path: string, value: any) {
     let delta = {
