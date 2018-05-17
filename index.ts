ba62bf9ea786ff3c885d93448e1dea8e0e625791
Jeremy Foster <jeremy.foster@live.com>
2018-05-17 14:24:16 -0500

    fix issues with websocket url, check in package.json

--- index.ts
+++ index.ts
@@ -1,23 +1,22 @@
-const five = require("johnny-five");
-const Raspi = require('raspi-io');
-const WebSocket = require('ws');
+// const five = require("johnny-five");
+// const Raspi = require('raspi-io');
+let WebSocket = require('ws');
 const config = require('./device.json');
+const argv = require('yargs').argv
 
 //setup websockets
-const ws = new WebSocket(config.webocketUrl);
+const ws = new WebSocket(config.websocketUrl);
 
 //setup j5
-let board = new five.Board({ io: new Raspi() });
-
+// let board = new five.Board({ io: new Raspi() });
 //ready the board and the websocket connection
-board.on("ready", () => {
-    ws.on('open', () => {
-
+// board.on("ready", () => {
+    ws.onopen = () => {
         //just send hard coded messages every second
         setInterval(() => sendDeltaMessage(config.deviceName, "environment.temperature", 68), 1000);
         setInterval(() => sendDeltaMessage(config.deviceName, "environment.humidity", 40), 1200);
-    });
-});
+    };
+// });
 
 function sendDeltaMessage(deviceId: string, path: string, value: any) {
     let delta = {
