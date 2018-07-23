e96bcd625a9c3558abd7dd11ddbe763194f5e981
Jeremy Foster <jeremy.foster@live.com>
2018-07-23 15:59:10 -0700

    created fileRelay and refactored

--- index.ts
+++ index.ts
@@ -1,24 +1,16 @@
 import { WebSocketClient } from "./WebSocketClient";
 import commandLineArgs from 'command-line-args';
-import moment from 'moment';
-
-let config = require('./device.json');
+import * as fileRelay from "./fileRelay";
+import * as sensorReader from "./sensorReader";
 import fs from 'fs';
-import path from 'path';
 
-
-require('dotenv').config();
-
-let sendTimers: any[] = [];
-
-//check that the file queue path exists
-const FILE_QUEUE_PATH = process.env.FILE_QUEUE_PATH || './queue';
-if (!fs.existsSync(FILE_QUEUE_PATH)) fs.mkdirSync(FILE_QUEUE_PATH);
+export let config = require('./device.json');
 
 //override config with command line options  
 let args = [
     { name: 'websocketUrl', alias: 'u', required: true },
-    { name: 'deviceName', alias: 'd', required: true }
+    { name: 'deviceName', alias: 'd', required: true },
+    { name: 'fileQueuePath', alias: 'q', required: true, defaultValue: './queue' }
 ];
 config = { ...config, ...commandLineArgs(args) };
 
@@ -27,88 +19,14 @@ args.filter(a => a.required && !config[a.name]).forEach(a => {
     throw new Error(`A ${a.name} argument must be provided either in a device.json file or as a command line argument (--${a.name} or -${a.alias}).`);
 })
 
-// const ws = new WS(config.websocketUrl);
-let client = new WebSocketClient(config.websocketUrl);
-client.on('open', () => {
-    switch (config.deviceName) {
-        case "rpz-cockpit":
-            mockSensor("environment/outside/temperature", 68, 5, 3000);
-            mockSensor("environment/outside/humidity", 40, 3, 3000);
-            break;
-        case "rpz-masthead":
-            mockSensor("environment/wind/speedApparent", 12, 5, 1000);
-            mockSensor("environment/outside/temperature", 55, 1, 3000);
-            mockSensor("environment/wind/directionTrue", 180, 45, 250);
-            mockSensor("environment/outside/humidity", 40, 3, 3000);
-            break;
-        case "rpz-engine":
-            mockSensor("environment/inside/temperature", 40, 3, 3000);
-            mockSensor("propulsion/mainEngine/coolantTemperature", 88, 1, 3000);
-            mockSensor("propulsion/mainEngine/intakeManifoldTemperature", 120, 1, 3000);
+//ensure the file queue path exists
+if (!fs.existsSync(config.fileQueuePath)) fs.mkdirSync(config.fileQueuePath);
 
-            break;
-        case "rpz-aftstar":
-            mockSensor("environment/inside/temperature", 68, 5, 3000);
-            mockSensor("environment/inside/humidity", 40, 3, 3000);
-            break;
-        case "rpz-aftport":
-            mockSensor("environment/inside/temperature", 68, 5, 3000);
-            mockSensor("environment/inside/humidity", 40, 3, 3000);
-            break;
-        case "rpz-master":
-            mockSensor("environment/inside/temperature", 68, 5, 3000);
-            mockSensor("environment/inside/humidity", 40, 3, 3000);
-            break;
-        case "rpz-cabin":
-            mockSensor("environment/inside/temperature", 68, 5, 3000);
-            mockSensor("environment/inside/humidity", 40, 3, 3000);
-            mockSensor("electrical/batteries/starter/voltage", 13.3, 0.2, 3000);
-            mockSensor("electrical/batteries/house/voltage", 13.1, 0.1, 3000);
-            mockSensor("electrical/batteries/starter/current", 0, 0.1, 3000);
-            mockSensor("electrical/batteries/house/current", 1.4, 0.3, 3000);
-            mockSensor("electrical/batteries/starter/temperature", 67, 0, 3000);
-            mockSensor("electrical/batteries/house/temperature", 67, 0, 3000);
-            break;
-        default:
-            console.log(`A device with the name ${config.deviceName} was not found.`);
-    }
-});
-client.on('close', () => {
-    sendTimers.forEach(interval => clearInterval(interval));
-    sendTimers = [];
-});
+//create websocket client
+export let client = new WebSocketClient(config.websocketUrl);
 
-//make it easy to create mock datapoints and send them as delta messages
-function mockSensor(datapoint: string, median: number, variance: number, frequency: number) {
-    let value;
-    sendTimers.push(setInterval(() => {
-        value = (median - variance) + Math.round(Math.random() * variance);
-        console.log(`Sending ${value} for ${datapoint}`);
-        let timestamp = moment().utc().format('YYYY-MM-DDTHHmmss.SSSS[Z]');
-        datapoint = datapoint.replace(/\//g, '_');
-        let filename = `${timestamp}_${datapoint}`;
+//start reading sensor data right away
+sensorReader.start();
 
-        let fullpath = path.join(FILE_QUEUE_PATH, filename);
-        fs.writeFileSync(fullpath, value, 'utf8');
-        // sendDeltaMessage(config.deviceName, datapoint, value)
-    }, frequency));
-
-}
-function sendDeltaMessage(deviceName: string, path: string, value: any) {
-    let delta = {
-        "updates": [
-            {
-                "source": {
-                    "deviceName": deviceName
-                },
-                "values": [
-                    {
-                        "path": path,
-                        "value": value
-                    }
-                ]
-            }
-        ]
-    };
-    client.send(JSON.stringify(delta));
-};
+client.on('open', () => fileRelay.start());
+client.on('close', () => fileRelay.stop());
